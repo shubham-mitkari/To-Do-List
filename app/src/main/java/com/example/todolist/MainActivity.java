3ffd0442e616f7ef2a5cefdf8c17db78f33802d1
Shubham Mitkari <shubmitkari@1996gmail.com>
2022-04-15 15:51:40 +0530

    Stage One Completed

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -38,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
+
 
         btnAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
